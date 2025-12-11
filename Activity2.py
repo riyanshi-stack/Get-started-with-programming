6890cee392e293fb7e8c37b23aaa2b69ed02ea93
riyanshi81 <nidhi.sinha81@gmail.com>
2025-12-11 18:30:34 +0530

    initial

---
+++ Activity2.py
@@ -0,0 +1,13 @@
+# creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# user input
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
